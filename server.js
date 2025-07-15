99e3190ead4e7a49418550d1e23dc80b0895cbf9
Kartheek Reddy Dalavai <kartheekreddydalavai@gmail.com>
2025-07-16 01:48:56 +0530

    adding cookies and working with tokens

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
+app.use(cookieParser()); // 👈 Add this line
 app.use('/', routes);
 
 app.listen(PORT, () => {
